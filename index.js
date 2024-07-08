8646dd18a201b4db0770c735d5dad97b1c474e49
NYP <5yvanpatrick@gmail.com>
2024-07-08 11:34:42 +0200

    change project file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const arr1 = ['Good morning', 'Good afternoon', 'Good evening', 'Good night'];
-const arr2 = ['You are lucky today', 'You are gonna be fine', 'You happiness is your priority', 'Never let your guard down', 'Keep thinking big'];
-const greetMessage = (arr1, arr2, num) =>{
-    let arrayMessage = arr1[Math.floor(Math.random() * arr1.length)];
-    let arrayLuck = arr2[Math.floor(Math.random() * arr2.length)];
+const messages ={
+    greetings: ['Good morning', 'Good afternoon', 'Good evening', 'Good night'],
+    fortunes: ['You are lucky today', 'You are gonna be fine', 'You happiness is your priority', 'Never let your guard down', 'Keep thinking big'],
+};
+
+const greetMessage = (message, num) =>{
+    let arrayMessage = messages.greetings[Math.floor(Math.random() * messages.greetings.length)];
+    let arrayLuck = messages.fortunes[Math.floor(Math.random() * messages.fortunes.length)];
     let degree = Math.floor(Math.random() * 30);
-    return `${arrayMessage}. ${arrayLuck}. now the degree is ${degree} degree Celsius`;
+    return `${arrayMessage}. \n${arrayLuck}. \nNow the degree is ${degree} degree Celsius`;
 }
 
-console.log(greetMessage(arr1,arr2));
+console.log(greetMessage(messages));
